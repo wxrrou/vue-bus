0c7c300c69ed17de2f2eb556c1987097dbb66cc9
wxrrou <wxrrou59@gmail.com>
2024-05-14 18:10:57 +0800

    0514

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import leaflet from 'leaflet';
 import { createApp } from 'vue';
 import VueAxios from 'vue-axios';
 import App from './App.vue';
-import router from "./router/index"; // 引入router
 import store from './store'; // 引入store
 import './style.css';
 
@@ -13,7 +12,6 @@ axios.defaults.baseURL = import.meta.env.VITE_APP_BASEURL
 
 createApp(App)
     .use(store) // 使用 store
-    .use(router) // 使用 router
     .use(VueAxios, axios, leaflet)
     .use(ElementPlus)
     .mount('#app')
